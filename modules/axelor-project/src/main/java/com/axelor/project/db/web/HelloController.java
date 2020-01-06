1c5d07d7f7b3459f9fa5f48f70d2cc36317cf49c
vishalbagde <vishalbagde.mmm@gmail.com>
2020-01-06 13:54:19 +0530

    set button status in event view

--- modules/axelor-project/src/main/java/com/axelor/project/db/web/HelloController.java
+++ modules/axelor-project/src/main/java/com/axelor/project/db/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.axelor.project.db.web;
 
 import com.axelor.meta.CallMethod;
+import com.axelor.project.db.Event;
+import com.axelor.project.db.EventStatus;
 import com.axelor.project.db.ProjectTask;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
@@ -11,23 +13,43 @@ public class HelloController {
 
   protected Logger log = LoggerFactory.getLogger(getClass());
 
-  @CallMethod
+  
   public String hello(String what) {
     System.out.println(what);
     return "Hello world " + what;
   }
 
-  @CallMethod
+  
   public void hello(ActionRequest request, ActionResponse response) {
     String str = "hello world";
     response.setFlash(str);
   }
 
-  @CallMethod
+  
   public void say(ActionRequest request, ActionResponse response) {
     ProjectTask projectTask = request.getContext().asType(ProjectTask.class);
     projectTask.setNotes("this is test note");
 
     response.setFlash(projectTask.getName());
   }
+  
+  
+  
+  public void changeStatus(ActionRequest request,ActionResponse response)
+  {
+	  
+	  Event event = request.getContext().asType(Event.class);
+	  
+	  String action=request.getAction();
+	  if(action.equals("action-project-event-method-onclick-change-statusPending"))
+		  response.setAttr("eventStatus","value", "PENDING");
+	  else if(action.equals("action-project-event-method-onclick-change-statusActive"))
+		  response.setAttr("eventStatus","value", "ACTIVE");
+	  else if(action.equals("action-project-event-method-onclick-change-statusDeactive"))
+		  response.setAttr("eventStatus","value", "DEACTIVE");
+	  else if(action.equals("action-project-event-method-onclick-change-statusClosed"))
+		  response.setAttr("eventStatus","value", "CLOSED"); 
+	  
+  }
+  
 }
